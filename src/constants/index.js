ca24175038e629567b32490520617e0db1c83291
Nathen Smith <nathencsmith@gmail.com>
2021-08-27 10:29:14 -0500

    push with some modifications and trying out spread operator

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const skills = ["React", "SQL", "MongoDB"];
+
+export default skills;
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 /* Your JS here. */
-console.log('Hello World!')
+console.log("Hello World!");
+
+import skills from "../constants";
+console.log(skills);
+// document.getElementById('output') = skills
+document.getElementById("output").innerHTML = [...skills];
+
+// export skills;
+document.body.style.backgroundColor = "blue";
